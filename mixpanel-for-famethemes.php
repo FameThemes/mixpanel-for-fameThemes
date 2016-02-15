61291e9063d296def90e7adc1b3427882fc042db
TruongSa <shrimp2t@gmail.com>
2016-02-15 21:06:21 +0700

    Use js for tracking purchase

--- mixpanel-for-famethemes.php
+++ mixpanel-for-famethemes.php
@@ -9,120 +9,72 @@
  * Text Domain:
  */
 
-// import Mixpanel
-require dirname( __FILE__ ). '/mixpanel/lib/Mixpanel.php';
+define( 'FT_MP_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+define( 'FT_MP_API_KEY', 'baffcc730f0885fb6e682a0d629b2fa0' );
 
-// get the Mixpanel class instance, replace with your project token
-// a513e89907b67abc5b3aea400e4e9707
-
-
-class FT_MP_Track_Payment {
-    public $user_id;
-    public $payment;
-    public $user = false;
-
-    function __construct( $payment_id , $status = false ) {
-
-
-        $total =  get_post_meta( $payment_id, '_edd_payment_total', true );
-        $customer_id =  get_post_meta( $payment_id, '_edd_payment_customer_id', true );
-        $meta =  get_post_meta( $payment_id, '_edd_payment_meta', true );
-        if ( ! is_array( $meta ) ) {
-            $meta = array();
-        }
-
-        if ( ! isset ( $meta['cart_details'] ) || ! is_array( $meta['cart_details'] ) || empty( $meta['cart_details']  )  ) {
-            return;
-        }
-
-       // var_dump(  $meta['cart_details'] );
-
-        //
-
-        //return ;
-
-
-        // $trans_id = edd_get_payment_transaction_id( $payment_id );
-        $mp = Mixpanel::getInstance("a513e89907b67abc5b3aea400e4e9707");
-        $total = floatval( $total );
-        if ( $status == 'publish' ){
-            foreach ( $meta['cart_details'] as $item ) {
-                $mp->track(
-                    "Purchase",
-                    array(
-                        "Amount" => floatval( $item['price'] ),
-                        'Product Name'=>  $item['name']
-                    )
-                );
-            }
-            // mixpanel.track("Purchase", {"Amount":100, "Product Name":"X", ... }};
-            // $mp->people->trackCharge( $customer_id, $total );
-            // $mp->people->append( $customer_id, "favorites", array("Baseball", "Reading"));
-        }
-
-        switch( $status ) {
-            case 'refunded':
-               // $mp->people->trackCharge( $customer_id, - $total );
-                foreach ( $meta['cart_details'] as $item ) {
-                    $mp->track( "Purchase", array( "Amount" => - floatval( $item['price'] ), 'Product Name'=>  $item['name'] ) );
-                }
-                break;
-            case 'failed':
-                break;
-        }
-
-
-        // track a custom "button click" event
-        ///$mp->track("button click", array("label" => "Login"));
-
-
-        // track an event
-        //$mp->track("button clicked", array("label" => "sign-up"));
-
-        //$author_obj = get_user_by('id', $this->user_id );
-        /*
-        switch( $status ) {
-            case 'refunded':
-                $this->process_refund();
-                break;
-            case 'failed':
-                $this->process_failure();
-                break;
-        }
-        */
-
-    }
-
-
-}
 
 class FT_MP {
 
     function __construct(){
-        add_action( 'edd_before_payment_status_change', array( __CLASS__, 'tracking_order' ), 35, 3 );
+       add_action( 'wp_footer',array( __CLASS__ , 'footer' ) );
     }
 
-    public static function tracking_order( $payment_id,  $status = '', $old_status = '' ){
-        new FT_MP_Track_Payment( $payment_id , $status, $old_status );
+    public static function footer() {
+        global $edd_receipt_args;
+        ?>
+        <!-- start Mixpanel -->
+        <script type="text/javascript">(function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
+                for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f)}})(document,window.mixpanel||[]);
+            mixpanel.init("<?php echo FT_MP_API_KEY; ?>");</script>
+        <!-- end Mixpanel -->
+        <?php
+        if ( ! $edd_receipt_args['id']  ) {
+            return ;
+        }
+        $sent =  EDD()->session->get( 'ft_mp_sent_'.$edd_receipt_args['id'] );
+        if ( $sent ) {
+           // return ;
+        }
+       // global $edd_receipt_args;
+        $success_page = edd_get_option( 'success_page' ) ? is_page( edd_get_option( 'success_page' ) ) : false;
+        if ( ! $success_page || ! edd_is_success_page() ){
+            return;
+        }
+        $session = edd_get_purchase_session();
+        $price = $session['price'];
+        $cart_items = $session['cart_details'];
+
+        if ( isset( $edd_receipt_args['id'] ) ) {
+            EDD()->session->set( 'ft_mp_sent_'.$edd_receipt_args['id'], true );
+        }
+        //  $session['user_info']['id']
+        ?>
+        <script type="text/javascript">
+
+            mixpanel.identify( <?php echo $session['user_info']['id'] ?> );
+            mixpanel.people.set( {
+                '$first_name' : <?php echo json_encode( $session['user_info']['first_name'] ); ?>,
+                '$last_name'  : <?php echo json_encode( $session['user_info']['last_name'] ); ?>,
+            });
+            mixpanel.people.track_charge( <?php echo floatval( $price ); ?> );
+
+            <?php
+            foreach ( $cart_items as $product ){
+            ?>
+            mixpanel.people.increment("Purchase Number", <?php echo intval( $product['quantity'] );  ?> );
+            mixpanel.people.append(
+                {
+                    "Product Name": <?php echo json_encode( $product["name"] ); ?>,
+                }
+            );
+            mixpanel.track("Purchase", {"Amount": <?php echo floatval( $product["item_price"] ) ?>, "Product Name":<?php echo json_encode( $product["name"] ); ?> });
+            <?php } ?>
+
+        </script>
+        <?php
     }
 
+    
 }
 
 new FT_MP();
-
-/*
-$mp = Mixpanel::getInstance("a513e89907b67abc5b3aea400e4e9707");
-
-// track an event
-$mp->track("button clicked", array("label" => "sign-up"));
-
-// create/update a profile for user id 12345
-$mp->people->set(12345, array(
-    '$first_name'       => "John",
-    '$last_name'        => "Doe",
-    '$email'            => "john.doe@example.com",
-    '$phone'            => "5555555555",
-    "Favorite Color"    => "red"
-));
-
-*/
